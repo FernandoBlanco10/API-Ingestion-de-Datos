c1751384493570536f4edf5f56703424edc62027
FernandoBlanco10 <blanco.alvarez.moy2002@gmail.com>
2025-12-08 13:45:22 -0600

    Adición de documentación openAPI

--- src/app.js
+++ src/app.js
@@ -4,6 +4,15 @@ const app = express();
 // Middleware global para parsear JSON
 app.use(express.json());
 
+// Swagger - OpenAPI
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const path = require("path");
+
+const swaggerDocument = YAML.load(path.join(__dirname, "./docs/openapi.yaml"));
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // Importar rutas
 const healthRoutes = require("./routes/health.routes");
 const ingestionRoutes = require("./routes/ingestion.routes");
